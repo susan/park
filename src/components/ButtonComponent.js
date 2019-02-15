041e58e31ba46e7ddd43844f676719a32b487730
susan <bluenote@outlook.com>
2019-02-15 15:08:03 -0500

    got fees on bookmark side

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -7,7 +7,7 @@ export default class ButtonComponent extends Component{
 
   render(){
   	return <button class="large circular ui icon button" onClick={()=> this.props.showParkDetails(this.props.park)}>  <i class="icon settings">
-        yes i am the button that shows details </i> </button>
+        Show Details </i> </button>
   }
 
 }
--- src/containers/BookmarkContainer.js
+++ src/containers/BookmarkContainer.js
@@ -23,22 +23,29 @@ export default class BookmarkContainer extends Component {
           <ParkCard key={park.id} park={park} handleBookMarked={this.props.handleBookMarked}
           handleParkDelete={this.props.handleParkDelete}
           showTextButton = {this.props.showTextButton}/>
-          <ButtonComponent park = {park} showParkDetails={this.props.showParkDetails}/>
+          <ButtonComponent park = {park} showParkDetails={this.showParkDetails}/>
         </div>
           )
 
         })
       return(
       <div>
-         parkList
+         <div>
+         {parkList}
+         </div>
+
+         {this.state.parkObj
+
+       &&
           <BookmarkCard parkObj={this.state.parkObj} />
+        }
       </div>
       )
      }
 
     render() {
-    	// fconsole.log("bookmarkcontainer props are", this.props)
-     //  console.log("--")
+    	console.log("bookmarkcontainer props are", this.props)
+       console.log("--")
 
       return (
       <div className="Container">
